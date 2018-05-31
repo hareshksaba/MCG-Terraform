86e1b951f4f5e8d27c79e0604f771277d4348be4
Haresh <hareshdevops@gmail.com>
2018-05-31 11:53:21 -0700

    single server setup ready

--- main.tf
+++ main.tf
@@ -1,8 +1,10 @@
 provider "aws"{
-    region = "us-east-1"
+    access_key = "AKIAJIUGPNZMM6QKZXBQ"
+    secret_key = "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"
+    region = "us-west-2"
 }
 
 resource "aws_instance" "test"{
-	ami = "ami-e251209a"
+	ami = "ami-db710fa3"
 	instance_type = "t2.micro"
 }
