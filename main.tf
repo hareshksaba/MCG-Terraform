be08b77ea24921da24077c8adfc857f599fb7af6
Haresh <hareshdevops@gmail.com>
2018-06-01 14:03:33 -0700

    Testing out Modules from Registry for S3 Website creation.

--- main.tf
+++ main.tf
@@ -1,10 +1,23 @@
-provider "aws"{
-    access_key = "AKIAJIUGPNZMM6QKZXBQ"
-    secret_key = "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"
-    region = "us-west-2"
+provider "aws" {
+  access_key = "AKIAJIUGPNZMM6QKZXBQ"
+  secret_key = "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"
+  region     = "us-west-2"
 }
 
-resource "aws_instance" "test"{
-	ami = "ami-db710fa3"
-	instance_type = "t2.micro"
+resource "aws_instance" "test" {
+  ami           = "ami-db710fa3"
+  instance_type = "t2.micro"
+}
+
+module "website" {
+  source    = "git::https://github.com/cloudposse/terraform-aws-s3-website.git?ref=master"
+  namespace = "cp"
+  stage     = "prod"
+  name      = "app"
+  hostname  = "docs.prod.cloudposse.org"
+
+  deployment_arns = {
+    "arn:aws:s3:::principal1" = ["/prefix1", "/prefix2"]
+    "arn:aws:s3:::principal2" = [""]
+  }
 }
