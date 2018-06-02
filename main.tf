2a63ebc75f73762b0e60841a00a9c7ab34b41e9b
Haresh <hareshdevops@gmail.com>
2018-06-02 11:21:40 -0700

    adding local modules

--- main.tf
+++ main.tf
@@ -1,4 +1,5 @@
 provider "aws" {
+  version    = "~> 1.20"
   access_key = "AKIAJIUGPNZMM6QKZXBQ"
   secret_key = "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"
   region     = "us-west-2"
@@ -9,7 +10,7 @@ resource "aws_instance" "test" {
   instance_type = "t2.micro"
 }
 
-module "website" {
+/*module "website" {
   source    = "git::https://github.com/cloudposse/terraform-aws-s3-website.git?ref=master"
   namespace = "cp"
   stage     = "prod"
@@ -20,4 +21,24 @@ module "website" {
     "arn:aws:s3:::principal1" = ["/prefix1", "/prefix2"]
     "arn:aws:s3:::principal2" = [""]
   }
+}*/
+
+module "aws_S3_module" {
+  source = "./aws_S3_module"
+}
+
+module "aws_LB_module" {
+  source = "./aws_LB_module"
+}
+
+module "aws_AutoScale_module" {
+  source = "./aws_AutoScale_module"
+}
+
+module "aws_EC2_module" {
+  source = "./aws_EC2_module"
+}
+
+module "aws_IAM_module" {
+  source = "./aws_IAM_module"
 }
