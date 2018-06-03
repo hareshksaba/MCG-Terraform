a579c69d299d4a29e578867fa6498c3cd9a09b86
Haresh <hareshksaba@gmail.com>
2018-06-03 12:31:02 -0700

    variables

---
+++ variables.tf
@@ -0,0 +1,23 @@
+variable "access_key" {
+  default = "AKIAI2CN6PLHYLTTRW7Q"
+}
+
+variable "secret_key" {
+  default = "/tKIS5I1UyjGD6bj8api5En2yEFHFpB6qynWZk1r"
+}
+
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "server_port" {
+  description = "The port the server will use for HTTP requests"
+  default     = 8080
+}
+
+variable "ami"{
+  default = "ami-db710fa3"
+} 
+
+            
+ 
