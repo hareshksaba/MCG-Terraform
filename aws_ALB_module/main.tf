0d6fbc712f77b0b3eba932d07a7ee65347f0ce8b
Haresh <hareshksaba@gmail.com>
2018-06-03 22:43:31 -0700

    adding comments

---
+++ aws_ALB_module/main.tf
@@ -0,0 +1,69 @@
+variable "lb_sg" {}
+
+data "aws_security_group" "selected" {
+  id = "${var.security_group_id}"
+}
+
+
+resource "aws_lb" "MCH-ALB" {
+  name               = "mch-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = ["${aws_security_group.lb_sg.id}"]
+  subnets            = ["${aws_subnet.public.*.id}"]
+
+  enable_deletion_protection = true
+
+  access_logs {
+    bucket  = "${data.aws_s3_bucket.lb_logs.bucket}"
+    prefix  = "mch-alb"
+    enabled = true
+  }
+
+  tags {
+    Environment = "production"
+  }
+}
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = "${aws_lb.MCH-ALB.arn}"
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-2015-05"
+  certificate_arn   = "arn:aws:iam::187416307283:server-certificate/test_cert_rab3wuqwgja25ct3n4jdj2tzu4"
+
+  default_action {
+    target_group_arn = "${aws_lb_target_group.MCH-ALB.arn}"
+    type             = "forward"
+  }
+}
+
+resource "aws_lb_listener_rule" "static" {
+  listener_arn = "${aws_lb_listener.front_end.arn}"
+  priority     = 100
+
+  action {
+    type             = "forward"
+    target_group_arn = "${aws_lb_target_group.static.arn}"
+  }
+
+  condition {
+    field  = "path-pattern"
+    values = ["/static/*"]
+  }
+}
+resource "aws_lb_target_group" "test" {
+  name     = "tf-example-lb-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = "${aws_vpc.main.id}"
+}
+
+resource "aws_vpc" "main" {
+  cidr_block = "10.0.0.0/16"
+}
+
+resource "aws_autoscaling_attachment" "asg_attachment_bar" {
+  autoscaling_group_name = "${aws_autoscaling_group.asg.id}"
+  elb                    = "${aws_elb.bar.id}"
+}
--- main.tf
+++ main.tf
@@ -1,3 +1,14 @@
+/*
+Code the following using modules
+A highly available autoscaling group
+Min 2 EC2s, max 5 EC2s in 2 different zones
+CPU at 85% triggers the scaling
+One S3 bucket
+IAM role to allow access to the S3 bucket
+Application Load Balancer with a target group that will contain the EC2s from the autoscaling group
+*/ 
+
+# Setting AWS as provider and setting default region using variables
 provider "aws" {
   version    = "~> 1.20"
   access_key = "${var.access_key}"
@@ -5,21 +16,34 @@ provider "aws" {
   region     = "${var.region}"
 }
 
+# Reads available availability zones
+
 data "aws_availability_zones" "available" {}
 
-/*module "aws_AutoScale_module" {
+# Using Modules to spin up services on AWS
+
+/*This module will create an autoscaling group
+Min 2 EC2s, max 5 EC2s in 2 different zones
+CPU at 85% triggers the scaling */
+
+module "aws_AutoScale_module" {
   source = "./aws_AutoScale_module"
-}*/
+}
+
+/*
+This module will create an S3 bucket 
+and create an IAM role to allow access to the above S3 bucket
+*/
 
 module "aws_S3_module" {
   source = "./aws_S3_module"
 }
 
+/*
+This module will create an Application Load Balancer 
+with a target group that will contain the EC2s from the above autoscaling group
+*/
 
-/*module "aws_IAM_module" {
-  source = "./aws_IAM_module"
-}*/
-
-/*module "aws_LB_module" {
-  source = "./aws_LB_module"
-}*/
+module "aws_ALB_module" {
+  source = "./aws_ALB_module"
+}
